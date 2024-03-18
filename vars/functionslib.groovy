e57e97e393b1ef041a12b7e4f43770c9b852b782
DevJaimeGR <140999790+DevJaimeGR@users.noreply.github.com>
2024-03-17 20:45:31 -0500

    Update functionslib.groovy

--- vars/functionslib.groovy
+++ vars/functionslib.groovy
@@ -1,6 +1,6 @@
 def pushDockerImage(String DOCKER_CRED_USR, String DOCKER_CRED_PSW, String IMAGE_NAME, String VERSION, String REGISTRY) {
     sh "docker login --username=${DOCKER_CRED_USR} --password=${DOCKER_CRED_PSW}"
-    ah "docker tag ${IMAGE_NAME}:${VERSION} ${REGISTRY}/${IMAGE_NAME}:${VERSION}"
+    sh "docker tag ${IMAGE_NAME}:${VERSION} ${REGISTRY}/${IMAGE_NAME}:${VERSION}"
     sh "docker push ${REGISTRY}/${IMAGE_NAME}:${VERSION}"
 }
 
