feb5e34ce76a84a46f164f31dc89412e90542781
DevJaimeGR <140999790+DevJaimeGR@users.noreply.github.com>
2024-03-17 20:00:49 -0500

    Update functionslib.groovy

--- vars/functionslib.groovy
+++ vars/functionslib.groovy
@@ -1,5 +1,3 @@
-// dockerlib.groovy
-
 def pushDockerImage(String DOCKER_CRED_USR, String DOCKER_CRED_PSW, String IMAGE_NAME, String VERSION, String REGISTRY) {
     sh """
     docker login --username=$DOCKER_CRED_USR --password=$DOCKER_CRED_PSW
